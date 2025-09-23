c39f93a976b5aed41ae1d05721978d27850ebd9a
16kwin <DKM_PSVU@mail.ru>
2025-09-23 20:22:43 +0500

    без входа

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import Menu from './components/menu';
+
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 document.documentElement.style.setProperty('--window-width', `${windowWidth}px`);
 document.documentElement.style.setProperty('--window-height', `${windowHeight}px`);
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Menu />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/menu" element={<Menu />} />
       </Routes>
     </Router>
